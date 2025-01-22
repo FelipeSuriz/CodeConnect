02f0131393f68a1c6f2b808297a357cd7923810f
Felipe Suriz <felipesuriz@hotmail.com>
2025-01-22 10:33:15 -0300

    promises

--- scripts.js
+++ scripts.js
@@ -3,4 +3,19 @@ const inputUpload = document.getElementById("image-upload");
 
 uploadBtn.addEventListener("click", () => {
     inputUpload.click();
-})
+});
+
+function lerConteudoDoArquivo(arquivo) {
+    return new Promise((resolve, reject) => {
+        const leitor = new FileReader();
+        leitor.onload = () => {
+            resolve({ url: leitor.result, nome: arquivo.name });
+        }
+
+        leitor.onerror = () => {
+            reject(`Erro na leitura do arquivo ${arquivo.name}`);
+        }
+
+        leitor.readAsDataURL(arquivo);
+    });
+}
